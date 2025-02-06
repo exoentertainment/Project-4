04f1ccd56f9fe382c1cd121b97f9fbac16d5d0ee
ExoEntertainment <perezronald09@gmail.com>
2025-02-05 20:55:39 -0800

    Created camera manager, planet camera, started weapon manager pane

---
+++ Assets/Managers/Camera Manager.cs
@@ -0,0 +1,11 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    #region --Serialized Fields--
+
+    [SerializeField] CinemachineCamera planetCamera;
+
+    #endregion
+}
---
+++ Assets/Managers/Input Manager.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+
+    public void OpenSettings(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            Debug.Log("Open Settings");
+        }
+    }
+}
